51e2c2cc4cd0ba3dc958ab1862921cb3cc480fb1
matheus <mrieper87@gmail.com>
2025-08-13 07:36:09 -0300

    fix in docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
 
-# Criar diretórios de logs
+# Criar diretórios de logs do Supervisor e da aplicação
 mkdir -p /var/www/storage/logs
 chown -R www-data:www-data /var/www/storage
-
-# Rodar scripts do composer agora que o container tá pronto
-composer install --no-interaction --prefer-dist --optimize-autoloader
-php artisan package:discover --ansi || true
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
+#chmod -R 777 /var/www/
 
 # Iniciar o Supervisor
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
